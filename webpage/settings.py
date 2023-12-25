19da2970bb5e315941feb827cfc2a045ecb6bffd
satya615 <139545203+satya615@users.noreply.github.com>
2023-12-25 14:10:08 +0530

    static and urls

--- webpage/settings.py
+++ webpage/settings.py
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY='8082097011a05335f9533aa184b0798c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = os.environ.get("DEBUG",False).lower == "true"
+# DEBUG=False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS=['*']
 
 
 # Application definition
@@ -121,8 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'  # The URL prefix for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # The absolute filesystem path to the directory that will hold your media files
-
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
