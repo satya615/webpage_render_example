ff6d8bb41709a336578c1236535a5143d1998ea6
satya615 <139545203+satya615@users.noreply.github.com>
2023-12-28 19:49:21 +0530

    form

--- webpage/settings.py
+++ webpage/settings.py
@@ -22,16 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECRET_KEY='8082097011a05335f9533aa184b0798c'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY='8082097011a05335f9533aa184b0798c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get("DEBUG","False").lower()=="true"
-# DEBUG=False
+# DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+DEBUG=False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS=['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS=['*']
 
 
 # Application definition
