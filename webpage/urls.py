cf54c0d5fcef9c9a9562073ab761d4d882f75650
satya615 <139545203+satya615@users.noreply.github.com>
2023-12-28 19:59:29 +0530

    form

--- webpage/urls.py
+++ webpage/urls.py
@@ -26,7 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('signup/', views.singup),
-    path('login/', views.user_login),
+    path('login/', views.login),
     path('', views.home),
   
      path('admin/', admin.site.urls), 
